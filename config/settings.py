066932611b30c69427d430ae76c249ba05a6b2f3
ch3rk4 <pety.shurupov@gmail.com>
2025-05-13 13:46:30 +0300

    create pastes

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,8 @@
+from pathlib import Path
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
@@ -7,4 +12,22 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'catalog',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [
+            BASE_DIR / 'templates',
+        ],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
 ]
